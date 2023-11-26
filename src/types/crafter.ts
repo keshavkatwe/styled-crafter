33beb4488159ccb72fc9423011a39848ea23d5a3
Keshav Katwe <keshav@investsky.com>
2023-11-26 21:47:28 +0400

    Added variant

--- src/types/crafter.ts
+++ src/types/crafter.ts
@@ -2,5 +2,12 @@ declare module 'styled-crafter' {
   export interface DefaultCrafter {
     spacing: string;
     color: string;
+    buttons: Record<
+      'primary' | 'secondary',
+      {
+        'background-color': string;
+        color: string;
+      }
+    >;
   }
 }
