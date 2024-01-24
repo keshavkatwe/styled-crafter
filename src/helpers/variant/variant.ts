ff0b1431254355d2afbba29bd779c58810049d76
Keshav Katwe <keshav@investsky.com>
2024-01-24 10:42:37 +0400

    fxied types for varianr

--- src/helpers/variant/variant.ts
+++ src/helpers/variant/variant.ts
@@ -1,9 +1,12 @@
 import { type IVariantFunctionProps } from './variant.types';
 import type IThemeVariant from '../../types/IThemeVariant';
+import type ICssValue from '../../types/ICssValue';
 
 const variant =
   ({ themeProperty, prop = 'variant' }: IVariantFunctionProps) =>
-  (props: Record<string, unknown> & IThemeVariant) => {
+  (
+    props: Record<string, unknown> & IThemeVariant,
+  ): Record<string, ICssValue> | undefined => {
     const themeObject = props?.theme?.[themeProperty];
     const propValue = props[prop] as string;
 
