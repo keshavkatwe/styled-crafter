f45aa473d75e315171dc62af692f94d19d5c0e6d
Keshav Katwe <keshav.katwe@gmail.com>
2024-02-29 22:23:44 +0400

    spit colors

--- src/helpers/colors/colors.ts
+++ src/helpers/colors/colors.ts
@@ -1,17 +1,48 @@
-import type ICssValue from '../../types/ICssValue';
-import { core } from '../core';
-import { type IColorProps } from './colors.types';
+import {
+  type IBackgroundColorProps,
+  type IColorProps,
+  type ITextColorProps,
+} from './colors.types';
+import { core2 } from '../core2';
+import { type IPropertyConfig, type IPropertyKey } from '../core2/core2.types';
 
-const coreColorInstance = core<Omit<IColorProps, 'theme'>>({
-  $color: 'color',
-  $clr: 'color',
-  $bgColor: 'background-color',
-  $backgroundColor: 'background-color',
+const textColorsConfig: Record<
+  IPropertyKey<Omit<ITextColorProps, 'theme'>>,
+  IPropertyConfig
+> = {
+  $color: { property: 'color', scale: 'colors' },
+  $clr: { property: 'color', scale: 'colors' },
+};
+
+const backgroundColorsConfig: Record<
+  IPropertyKey<Omit<IBackgroundColorProps, 'theme'>>,
+  IPropertyConfig
+> = {
+  $backgroundColor: { property: 'background-color', scale: 'colors' },
+  $bgColor: { property: 'background-color', scale: 'colors' },
+};
+
+export const textColors =
+  core2<Omit<ITextColorProps, 'theme'>>(textColorsConfig);
+export const backgroundColors = core2<Omit<IBackgroundColorProps, 'theme'>>(
+  backgroundColorsConfig,
+);
+
+const colors = core2<Omit<IColorProps, 'theme'>>({
+  ...textColorsConfig,
+  ...backgroundColorsConfig,
 });
 
-const colors = (props: IColorProps): Record<string, ICssValue> => {
-  return coreColorInstance(props, {
-    themeAccessKey: 'colors',
-  });
-};
+// const coreColorInstance = core2<Omit<IColorProps, 'theme'>>({
+//   $color: 'color',
+//   $clr: 'color',
+//   $bgColor: 'background-color',
+//   $backgroundColor: 'background-color',
+// });
+//
+// const colors = (props: IColorProps): Record<string, ICssValue> => {
+//   return coreColorInstance(props, {
+//     themeAccessKey: 'colors',
+//   });
+// };
 export default colors;
--- src/helpers/colors/colors.types.ts
+++ src/helpers/colors/colors.types.ts
@@ -2,9 +2,15 @@ import { type DefaultCrafter } from 'styled-crafter';
 import type ITheme from '../../types/ITheme';
 
 type IColor = DefaultCrafter['colors'];
-export interface IColorProps extends ITheme {
+
+export interface ITextColorProps extends ITheme {
   $color?: IColor;
   $clr?: IColor;
+}
+
+export interface IBackgroundColorProps extends ITheme {
   $backgroundColor?: IColor;
   $bgColor?: IColor;
 }
+
+export type IColorProps = ITextColorProps & IBackgroundColorProps;
