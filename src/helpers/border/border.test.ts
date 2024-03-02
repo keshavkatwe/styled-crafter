765681e1fbb88f7958c937a33b62e59c961ba623
Keshav Katwe <keshav.katwe@gmail.com>
2024-03-02 16:17:16 +0400

    Added border test cases

--- src/helpers/border/border.test.ts
+++ src/helpers/border/border.test.ts
@@ -1,11 +1,58 @@
-import { borderRadius, type IBorderRadiusProps } from './index';
+import {
+  border,
+  borderOther,
+  borderRadius,
+  borderWidth,
+  IBorderOtherProps,
+  type IBorderRadiusProps,
+} from './index';
 
 describe('should check border test cases', () => {
+  it('should basic border', () => {
+    expect(
+      border({
+        $bw: '10px',
+      }),
+    ).toStrictEqual({
+      'border-width': '10px',
+    });
+  });
+
   it('should test border radius ', () => {
     expect(
-      borderRadius({ $borderRadius: '10px' } satisfies IBorderRadiusProps),
+      borderRadius({
+        $borderRadius: '10px',
+        $btlr: 'br2',
+        theme: {
+          radii: {
+            br1: '20px',
+            br2: '30px',
+          },
+        },
+      } satisfies IBorderRadiusProps),
     ).toStrictEqual({
       'border-radius': '10px',
+      'border-top-left-radius': '30px',
+    });
+  });
+
+  it('should basic border width', () => {
+    expect(
+      borderWidth({
+        $bw: '10px',
+      }),
+    ).toStrictEqual({
+      'border-width': '10px',
+    });
+  });
+
+  it('should basic border other props', () => {
+    expect(
+      borderOther({
+        $borderStyle: 'solid',
+      } satisfies IBorderOtherProps),
+    ).toStrictEqual({
+      'border-style': 'solid',
     });
   });
 });
